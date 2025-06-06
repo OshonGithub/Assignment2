a3ec39e6492aab45b1a9e72925c63c4ffaf99fd4
OshonGithub <70640393+OshonGithub@users.noreply.github.com>
2025-06-06 16:54:27 +1000

    Add files via upload

---
+++ assignment2_CSIT375/adaptive_attack.py
@@ -0,0 +1,19 @@
+import torch
+
+
+
+def transform(
+        x: torch.Tensor,
+        exp_cfg,
+) -> torch.Tensor:
+    """
+    x: normalized images of shape (N, C, H, W)
+    exp_cfg: general configurations including out_dir, data_dir, device, etc.
+
+    return: transformed images of shape (N, C, H, W)
+    """
+
+    raise NotImplementedError
+
+
+
